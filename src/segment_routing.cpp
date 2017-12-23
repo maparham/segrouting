594ad8adfcf2cc56a43e993e45640efa62a7bb6a
Mahmoud Parham <mahmoud@h6710.eduroam-5.wlan.aalborguniversitet.dk>
2017-12-23 19:02:06 +0100

    precomputation

--- src/segment_routing.cpp
+++ src/segment_routing.cpp
@@ -1,14 +1,41 @@
 #include <iostream>
 #include "Snap.h"
 #include <fstream>
-#include <unordered_map>
+//#include <unordered_map>
+#include <algorithm>    // std::reverse
 #include <vector>
-
+#include <map>
+#include <time.h>
 
 using namespace std;
 using namespace TSnap;
 
 #define DEBUG 1
+#define INF numeric_limits<int>::max()/3
+#define WEIGHTATTR "weight"
+
+//// what type of graph do you want to use?
+typedef PNEANet MyGraph; // undirected graph
+//typedef PNGraph PGraph;  //   directed graph
+//typedef PNEGraph PGraph;  //   directed multigraph
+//typedef TPt<TNodeNet<TInt> > PGraph;
+//typedef TPt<TNodeEdgeNet<TInt, TInt> > PGraph;
+typedef vector<int> Path;
+typedef const int MyNode;
+//// what type of graph do you want to use?
+typedef PNEANet MyGraph; // undirected graph
+//typedef PNGraph PGraph;  //   directed graph
+//typedef PNEGraph PGraph;  //   directed multigraph
+//typedef TPt<TNodeNet<TInt> > PGraph;
+//typedef TPt<TNodeEdgeNet<TInt, TInt> > PGraph;
+
+struct Segment {
+	bool isNode; // otherwise link
+	int id;
+};
+typedef vector<Segment> SegmentStack;
+typedef map<int, SegmentStack> BackupPaths; // key is link id
+typedef map<int, BackupPaths> Dest_Link_Table; // key is destination id
 
 struct Logger: std::ostream {
 	template<typename T>
@@ -21,92 +48,11 @@ struct Logger: std::ostream {
 	}
 } mylog;
 
-int GetWeightedShortestPath11(const PNEANet Graph, const int& SrcNId,
-		TIntFltH& NIdDistH, const TFltV& Attr) {
-	TIntV frontier;
-
-	NIdDistH.Clr(false);
-	NIdDistH.AddDat(SrcNId, 0);
-	frontier.Add(SrcNId);
-	while (!frontier.Empty()) {
-		const int NId = findMinimum(frontier, NIdDistH);
-		printf("\nNId=%d", NId);
-		const PNEANet::TObj::TNodeI NodeI = Graph->GetNI(NId);
-		for (int v = 0; v < NodeI.GetOutDeg(); v++) {
-			int DstNId = NodeI.GetOutNId(v);
-			int EId = NodeI.GetOutEId(v);
-
-			float elen = Attr[EId];
-			if (!NIdDistH.IsKey(DstNId)) {
-				NIdDistH.AddDat(DstNId, NIdDistH.GetDat(NId) + Attr[EId]);
-				frontier.Add(DstNId);
-				printf("\nNIdDistH.AddDat(%d,  Attr[%d]=%f);\n", DstNId, EId,
-						NIdDistH.GetDat(NId) + Attr[EId]);
-
-			} else {
-				printf("\nNIdDistH.GetDat(%d)=%f", DstNId,
-						NIdDistH.GetDat(DstNId));
-				printf("\nNIdDistH[%d]=%f \n", DstNId,
-						NIdDistH[DstNId]);
-				printf(
-						"for node %d, testing the parent %d, new distance=%f, the current dist=%f\n",
-						DstNId, NId, NIdDistH.GetDat(NId) + Attr[EId],
-						NIdDistH.GetDat(DstNId));
-
-				if (NIdDistH[DstNId] > NIdDistH.GetDat(NId) + Attr[EId]) {
-					NIdDistH[DstNId] = NIdDistH.GetDat(NId) + Attr[EId];
-					printf("DstNId=%d NIdDistH[DstNId]=%f", DstNId,
-							NIdDistH[DstNId]);
-				}
-			}
-		}
-	}
-	return 0;
-}
-
-int main() {
-
-//	ifstream topgen;
-//	topgen.open("/Users/mahmoud/Downloads/weights-dist/1221/latencies.intra");
-
-//	if (!topgen.is_open()) {
-//		mylog("file not opened.");
-//		return nodes;
-//	}
-
-//// what type of graph do you want to use?
-	typedef PNEANet MyGraph; // undirected graph
-	//typedef PNGraph PGraph;  //   directed graph
-	//typedef PNEGraph PGraph;  //   directed multigraph
-	//typedef TPt<TNodeNet<TInt> > PGraph;
-	//typedef TPt<TNodeEdgeNet<TInt, TInt> > PGraph;
-
-//	G= TSnap::GenRndGnm<MyGraph>(10,20);
-
-	const TStr weightAttr = "weight", inFile =
-			"/Users/mahmoud/Downloads/weights-dist/1221/latencies.intra",
-			inFile1 = "in.txt";
-	// load graph from file
-	TStrHash<TInt> InStrToNIdH;
-	MyGraph G = LoadWeightedEdgeListStr<MyGraph>(inFile, 0, 1, 2, weightAttr,
-			InStrToNIdH);
-
-	TIntStrH NodeLabelH;
-	int dest, src = 0;
-	for (auto NI = G->BegNI(); NI < G->EndNI(); NI++) {
-		NodeLabelH.AddDat(NI.GetId(), InStrToNIdH.GetKey(NI.GetId()));
-		dest = NI.GetId();
-	}
-
-	DrawGViz(G, TGVizLayout::gvlDot, "mygraph.png", "Loaded Graph", true);
-	DrawGViz(G, TGVizLayout::gvlDot, "mygraph_labeled.png", "Loaded Graph",
-			NodeLabelH);
-
-//	int EId = G->GetEI(src, 1).GetId();
-//	 G->AddIntAttrDatE(EId, 9999999, weightAttr);
-
+template<class Graph>
+int getSP(Graph G, MyNode src, MyNode dest, Path& sp) {
+	sp.clear();
 	TIntH SP_tree;
-	GetWeightedShortestPathTree(G, src, SP_tree, weightAttr);
+	GetWeightedShortestPathTree(G, src, SP_tree, WEIGHTATTR);
 
 //	TIntFltH NIdDistH;
 //	TFltV Attr;
@@ -121,16 +67,159 @@ int main() {
 
 	int N = dest;
 	int spLen = 0;
-	vector<int> sp;
 	while (SP_tree.IsKey(N) && N != src) {
 		sp.push_back(N); // save the shortest path
-		mylog << N << ",";
 		int parent = SP_tree.GetDat(N);
-
 		int EID = G->GetEI(parent, N).GetId();
-		spLen += G->GetIntAttrDatE(EID, weightAttr);
+		spLen += G->GetIntAttrDatE(EID, WEIGHTATTR);
 		N = parent;
 	}
-	mylog << "\nspLen=" << spLen;
+	sp.push_back(src);
+	std::reverse(sp.begin(), sp.end());
+	return spLen;
+}
+
+void printsp(Path& sp) {
+	for (int i = 0; i < sp.size() - 1; ++i) {
+		printf("%d,", sp[i]);
+	}
+	if (sp.size()) {
+		printf("%d\n", sp[sp.size() - 1]);
+	}
+}
+
+bool inPSpace(MyGraph& G, MyNode node, MyNode S, MyNode F) {
+	Path sp_;
+	getSP(G, S, node, sp_);
+	for (int i = 1; i < sp_.size(); ++i) {
+		if (sp_[i - 1] == S && sp_[i] == F) {
+			printf("%d not in PSpace; ", node);
+			printf("SP(S, %d)=", node);
+			printsp(sp_);
+			return false;
+		}
+	}
+	return true;
+}
+
+bool inQSpace(MyGraph& G, MyNode node, MyNode S, MyNode F, MyNode D) {
+	Path sp_;
+	getSP(G, node, D, sp_);
+	for (int i = sp_.size() - 2; i > -1; --i) {
+		if (sp_[i] == S && sp_[i + 1] == F) {
+			printf("%d not in QSpace; ", node);
+			printf("SP(%d,D)=", node);
+			printsp(sp_);
+			return false;
+		}
+	}
+	return true;
+}
+
+int main() {
+	/* initialize random seed: */
+	srand(time(NULL));
+//	ifstream topgen;
+//	topgen.open("/Users/mahmoud/Downloads/weights-dist/1221/latencies.intra");
+
+//	if (!topgen.is_open()) {
+//		mylog("file not opened.");
+//		return nodes;
+//	}
+
+//	G= TSnap::GenRndGnm<MyGraph>(10,20);
+
+	const TStr inFile =
+			"/Users/mahmoud/Downloads/weights-dist/3967/latencies.intra",
+			inFile1 = "in.txt";
+// load graph from file
+	TStrHash<TInt> InStrToNIdH;
+	MyGraph G = LoadWeightedEdgeListStr<MyGraph>(inFile, 0, 1, 2, WEIGHTATTR,
+			InStrToNIdH);
+	int nofLinks = G->GetEdges();
+	int nofNodes = G->GetNodes();
+	printf("Loaded graph: %d nodes, %d links\n", nofNodes, nofLinks);
+
+	TIntStrH NodeLabelH;
+	int dest;
+	for (auto NI = G->BegNI(); NI < G->EndNI(); NI++) {
+		NodeLabelH.AddDat(NI.GetId(), InStrToNIdH.GetKey(NI.GetId()));
+		dest = NI.GetId();
+	}
+
+	DrawGViz(G, TGVizLayout::gvlDot, "mygraph.png", "Loaded Graph", true);
+	DrawGViz(G, TGVizLayout::gvlDot, "mygraph_labeled.png", "Loaded Graph",
+			NodeLabelH);
+
+	Dest_Link_Table table;
+	// TODO iterate over all destinations
+	BackupPaths bp; // for a fixed destination
+	table[dest] = bp;
+
+	for (TNEANet::TEdgeI EI = G->BegEI(); EI < G->EndEI(); EI++) {
+		int S = EI.GetSrcNId();
+		int F = EI.GetDstNId();
+		int D = dest; // iterate on all possible destinations
+		printf("link (%d, %d), D=%d\n", S, F, D);
+
+		Path sp;
+		int len = getSP(G, S, D, sp);
+		if (len >= INF) {
+			mylog << "D is not reachable from S\n";
+			continue;
+		}
+		printf("first shortest path weight=%d: ", len);
+		printsp(sp);
+		if (sp[1] != F) {
+			mylog << "SP not using the link\n";
+			continue;
+		}
+
+		const int w = G->GetIntAttrDatE(EI.GetId(), WEIGHTATTR);
+		G->AddIntAttrDatE(EI.GetId(), INF, WEIGHTATTR); // cripple the link (S,F)
+		len = getSP(G, S, D, sp); // shortest path without (S,F) => post convergence path
+		G->AddIntAttrDatE(EI.GetId(), w, WEIGHTATTR);
+		printf("second shortest path weight=%d: ", len);
+		printsp(sp);
+		if (len >= INF) {
+			mylog << "no backup path\n";
+			continue;
+		}
+
+		int Pspace_sup = S, Qspace_inf = D;
+		for (int i = 0; i < sp.size() && inPSpace(G, sp[i], S, F); ++i) {
+			printf("%d in PSpace\n", sp[i]);
+			Pspace_sup = i;
+		}
+		for (int i = sp.size() - 1;
+				i >= Pspace_sup && inQSpace(G, sp[i], S, F, D); --i) { // search backward for the infimum of QSpace, stop when it touches the PSpace
+			printf("%d in QSpace\n", sp[i]);
+			Qspace_inf = i;
+		}
+
+		Assert(Qspace_inf == Pspace_sup + 1 || Pspace_sup == Qspace_inf);
+		MyNode p = sp[Pspace_sup], q = sp[Qspace_inf];
+		printf("p=%d q=%d\n", p, q);
+		// extract the segments from p and q
+
+		Segment seg = { true, p };
+		SegmentStack stack = { seg };
+		table[dest][EI.GetId()] = stack;
+		if (Qspace_inf > Pspace_sup) { // also push a "tunnel link"
+			table[dest][EI.GetId()].push_back( { false, G->GetEId(p, q) });
+		}
+	}
+
+	printf("\nmap size for dest node=%u\n", table[dest].size());
+	printf("table size=%u\n", table.size());
+
+//	int randNode = sp[rand() % (sp.size() - 1)];
+//	printf("randNode=%d", randNode);
+//	int failedLink = G->GetEI(sp[randNode], sp[randNode + 1]).GetId();
+//	printf("failedLink=%d\n", failedLink);
+//	G->AddIntAttrDatE(failedLink, INF,WEIGHTATTR);
+//	getSP(G, src, dest, sp);
+//	printsp(sp);
+
 	return 0;
 }
